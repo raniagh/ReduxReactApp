72d038ee9704053f48fb9d61f61563c7c793eb8d
Your Name <rghzaiel@ggn-group.com>
2023-02-15 16:56:29 +0100

    redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,33 @@
 import { Component } from "react";
 import { connect, useDispatch, useSelector } from "react-redux";
+import { counterActions } from "../store";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
-
   //when using useSelector react redux will set automatically a subscription to the redux store
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const dispatch = useDispatch();
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
+  //5 is affected to the payload property
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle());
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,65 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { legacy_createStore as createStore } from "redux";
-const counterReducer = (state = { counter: 0 }, action) => {
+
+const initialState = { counter: 0, showCounter: true };
+
+//A slice of global state
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      //not changing into the original state
+      //It's immutable
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+//configureStore merge all reducers into a global one
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+export const counterActions = counterSlice.actions;
+
+export default store;
+
+/* const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
   return state;
 };
 
-const store = createStore()(counterReducer);
-
-export default store;
+const store = createStore(counterReducer); */
